7407e164e89d80ffc24e5dd9dcd12ba20447baf0
Elizabeth Hanson <emhanson08@gmail.com>
2017-10-04 23:02:19 +0000

    Completed Lesson 27

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003215429) do
+ActiveRecord::Schema.define(version: 20171004215341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20171003215429) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id", using: :btree
+    t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", using: :btree
   end
 
   create_table "lessons", force: :cascade do |t|
