ec1662400a61922729a56c0756ca1901e502ea82
Elizabeth Hanson <emhanson08@gmail.com>
2017-10-04 23:27:25 +0000

    Completed Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,11 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+   #redirect user to root_url with an error message if they are not enrolled in the course
+
   def show
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to root_url, alert: "You are not enrolled in this course"
+    end
   end
 
   private
